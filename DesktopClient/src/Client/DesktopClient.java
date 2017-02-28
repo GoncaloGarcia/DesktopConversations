8e9cd5139847d7b63f1442edf177125a7a0c5051
GGfpc <ggfpc12495@gmail.com>
2017-02-28 23:31:58 +0000

    Basic Layout

--- DesktopClient/src/Client/DesktopClient.java
+++ DesktopClient/src/Client/DesktopClient.java
@@ -3,45 +3,61 @@ package Client;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class DesktopClient extends Application{
-	
-	@Override
-	public void start(Stage primaryStage){
-		Button btn = new Button();
-		btn.setText("OLA");
-		Button btn2 = new Button();
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-			
-			@Override
-			public void handle(ActionEvent arg0) {
-				btn.setText(btn.getText() + " Ola");
-				
-			}
-		});
-		
-		BorderPane root = new BorderPane();
-		
-		root.setTop(btn);
-		root.setCenter(btn2);
-		root.setAlignment(btn, Pos.TOP_CENTER);
-		
-		Scene scene = new Scene(root,300,250);
-		
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-	}
-	
+public class DesktopClient extends Application {
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Conversations.im Desktop");
+		BorderPane bPane = new BorderPane();
+		bPane.setId("bpane");
+		
+		
+		FlowPane rightPane = new FlowPane();
+		rightPane.setId("rpane");
+		rightPane.setPrefSize(600,500);
+		
+		BorderPane leftPane = new BorderPane();
+		leftPane.setId("lpane");
+		leftPane.setPrefWidth(250);
+		
+		HBox userTopPane = new HBox();
+		userTopPane.setId("userTopPane");
+		userTopPane.setMinSize(100,100);
+		
+		leftPane.setTop(userTopPane);
+		
+		bPane.setRight(rightPane);
+		bPane.setLeft(leftPane);
+		
+		Scene scene = new Scene(bPane);
+		scene.getStylesheets().add(DesktopClient.class.getResource("desktopclient.css").toExternalForm());
+		
+		
+		
+		
+		primaryStage.sizeToScene();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 }
